0272eea257c85ada610c08ae0c06308ce37cfc86
Ethantl28 <ethantl28@outlook.com>
2022-11-19 22:55:16 +0000

    Added 2 different types of triangle and a square that extends from the middle.
    The square needs some more optimisation, I could not figure out how to move the start and end points into a for loop. This would also need to be put into a square drawing class for easy access.

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,47 @@ int main() //!< Entry point for the application
 	sf::RenderWindow window(sf::VideoMode(1024, 800), "Lab Book 1 - Shapes");
 	window.setFramerateLimit(60);
 
+	//Set up hollow triangle
+	int hollowSize = 3;
+	sf::VertexArray hollowTriangle;
+	hollowTriangle.resize(hollowSize + 1);						//The +1 on top of the size is because we have to have 4 points to make the triangle connect
+	hollowTriangle.setPrimitiveType(sf::LineStrip);		
+
+	//Sets start position and end position			//This is the code to make a triangle hollow
+	hollowTriangle[0] = sf::Vector2f(300, 100);
+	hollowTriangle[1] = sf::Vector2f(400, 200);
+	hollowTriangle[2] = sf::Vector2f(300, 200);
+	hollowTriangle[3] = hollowTriangle[0];
+
+
+
+	//Set up filled in triangle
+	int size = 3;
+	sf::VertexArray triangle;
+	triangle.resize(size);
+	triangle.setPrimitiveType(sf::TrianglesStrip);		//Changing to Sf::TriangleStrip makes a full triangle and fills it in 
+														
+	//Sets start position and end position
+	triangle[0] = sf::Vector2f(100, 100);
+	triangle[1] = sf::Vector2f(200, 200);
+	triangle[2] = sf::Vector2f(100, 200);
+
+
+
+	//Set up square that extends from middle
+	int squareSize = 4;
+	float squareLength = 100;
+	sf::Vector2f startPosition(200, 500);
+	sf::VertexArray square;
+	square.resize(squareSize + 1);
+	square.setPrimitiveType(sf::LineStrip);													//This would be inside of a class so it would not take up this much space in main.cpp
+
+	//Gets start point and extends from middle
+	square[0] = sf::Vector2f(startPosition.x - squareLength, startPosition.y - squareLength);
+	square[1] = sf::Vector2f(startPosition.x + squareLength, startPosition.y - squareLength);
+	square[2] = sf::Vector2f(startPosition.x + squareLength, startPosition.y + squareLength);
+	square[3] = sf::Vector2f(startPosition.x - squareLength, startPosition.y + squareLength);
+	square[4] = square[0];
 	
 	while (window.isOpen())
 	{
@@ -34,9 +75,11 @@ int main() //!< Entry point for the application
 		}
 
 		window.clear();
-
-		// Do your drawing here
 		
+		//Draw to screen
+		window.draw(hollowTriangle);
+		window.draw(triangle);
+		window.draw(square);
 
 		window.display();
 	}
